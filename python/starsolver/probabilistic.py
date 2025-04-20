011eaa2c2b93f38de90e99c34739d153a5612a1d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-20 11:55:38 +0100

    style: move ProbabilisticCell above ProbabilisticBoard

--- python/starsolver/probabilistic.py
+++ python/starsolver/probabilistic.py
@@ -7,6 +7,25 @@ from board import Board
 from cell import Cell, Coordinate
 
 
+class ProbabilisticCell(Cell):
+    def __init__(self, coord: Coordinate, p_star: float) -> None:
+        """A Cell with an assigned probability of being a star."""
+        assert (p_star <= 1) and (p_star >= 0)
+
+        super().__init__(coord)
+
+        self.p_star: float = p_star
+        self.p_dot: float = 1 - p_star
+
+    def __repr__(self) -> str:
+        return f'ProbabilisticCell({self.coord}, {self.p_star})'
+
+    @property
+    def probability(self) -> float:
+        """Gets the probability that this is a star."""
+        return self.p_star
+
+
 class ProbabilisticBoard(Board):
     def __init__(self):
         """A Board where each cell is a ProbabilisticCell that has an assigned probability of being a star."""
@@ -28,26 +47,7 @@ class ProbabilisticBoard(Board):
         for cell in self.cells:
             print(cell)
 
-        _matplotlib_template()
-
-
-class ProbabilisticCell(Cell):
-    def __init__(self, coord: Coordinate, p_star: float) -> None:
-        """A Cell with an assigned probability of being a star."""
-        assert (p_star <= 1) and (p_star >= 0)
-
-        super().__init__(coord)
-
-        self.p_star: float = p_star
-        self.p_dot: float = 1 - p_star
-
-    def __repr__(self) -> str:
-        return f'ProbabilisticCell({self.coord}, {self.p_star})'
-
-    @property
-    def probability(self) -> float:
-        """Gets the probability that this is a star."""
-        return self.p_star
+            # _matplotlib_template()
 
 
 # FIXME remove _matplotlib_template() once useful parts copied into plot_probability_surface()
