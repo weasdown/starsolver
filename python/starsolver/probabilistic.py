ec9e17f3f7083a2ab599ba00c12daec960de63cf
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-20 11:54:13 +0100

    feat: add placeholder ProbabilisticBoard.plot_probability_surface() method

--- python/starsolver/probabilistic.py
+++ python/starsolver/probabilistic.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
 from board import Board
 from cell import Cell, Coordinate
 
@@ -18,6 +23,13 @@ class ProbabilisticBoard(Board):
 
         self.cells: list[ProbabilisticCell] = p_cells
 
+    def plot_probability_surface(self) -> None:
+        """Plots a 3D surface showing the probability that each cell in the board is a star."""
+        for cell in self.cells:
+            print(cell)
+
+        _matplotlib_template()
+
 
 class ProbabilisticCell(Cell):
     def __init__(self, coord: Coordinate, p_star: float) -> None:
@@ -36,3 +48,35 @@ class ProbabilisticCell(Cell):
     def probability(self) -> float:
         """Gets the probability that this is a star."""
         return self.p_star
+
+
+# FIXME remove _matplotlib_template() once useful parts copied into plot_probability_surface()
+def _matplotlib_template():
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    # Make data.
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X ** 2 + Y ** 2)
+    Z = np.sin(R)
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(-1.01, 1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    # A StrMethodFormatter is used automatically
+    ax.zaxis.set_major_formatter('{x:.02f}')
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    p_board: ProbabilisticBoard = ProbabilisticBoard()
+    p_board.plot_probability_surface()
