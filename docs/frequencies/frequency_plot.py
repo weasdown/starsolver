9d20afef9c9623ddb54a57adf067cb19a1a8d36b
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-14 16:55:30 +0100

    feat: add frequency_plot.py

---
+++ docs/frequencies/frequency_plot.py
@@ -0,0 +1,17 @@
+# Plot of frequency components for the Easy #1 board from the Star Battle app.
+
+import matplotlib.pyplot as plt
+
+frequency_components = [2 ** i for i in range(9)]
+print(f'{frequency_components = }')
+
+coefficients = [130, 40, 257, 80, 5, 160, 10, 320, 20]
+print(f'{coefficients = }')
+
+ax = plt.axes()
+plt.plot(frequency_components, coefficients)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Coefficient')
+ax.set_xscale('log', base=2)
+
+plt.show()
